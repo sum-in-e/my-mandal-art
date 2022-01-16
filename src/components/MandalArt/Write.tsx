66a4bbbc7e8901bd795c56f6932e8fa27701d299
sumin <suminkim.me@gmail.com>
2022-01-16 16:22:35 +0900

    Add write page

---
+++ src/components/MandalArt/Write.tsx
@@ -0,0 +1,8 @@
+import { Container } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+
+const Write: FunctionComponent = () => {
+  return <Container>write 페이지</Container>;
+};
+
+export default Write;
--- src/pages/mandalArt/write.tsx
+++ src/pages/mandalArt/write.tsx
@@ -1,7 +1,8 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+
+import Write from 'components/MandalArt/Write';
 
 const WriteMandalArt: NextPage = () => {
   const router = useRouter();
@@ -10,15 +11,11 @@ const WriteMandalArt: NextPage = () => {
     <div>
       <Head>
         <title>My Mandal Art | Write</title>
-        <meta name="description" content="Mandal Art" />
+        <meta name="description" content="Mandal-Art" />
       </Head>
 
       <main>
-        <Box>
-          <Heading as="h1" size="4xl" color="highlight">
-            Mandal Art를 만들어보세요!
-          </Heading>
-        </Box>
+        <Write />
       </main>
     </div>
   );
