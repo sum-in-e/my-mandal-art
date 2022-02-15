fd90ce6a57cf3d874c91d025c78755134c10dcb3
sumin <suminkim.me@gmail.com>
2022-02-15 22:11:30 +0900

    Add 서버사이드에서 verifyToken 하도록 추가 (1차 작업)

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,9 +1,25 @@
 import { Box, Heading, Text } from '@chakra-ui/react';
-import type { NextPage } from 'next';
+import type {
+  GetServerSideProps,
+  NextPage,
+  InferGetServerSidePropsType,
+} from 'next';
 import Head from 'next/head';
 import { Signin } from 'components/Auth';
+import nookies from 'nookies';
+import { getAuth } from 'firebase-admin/auth';
+import { useRouter } from 'next/router';
 
-const SigninPage: NextPage = () => {
+const SigninPage: NextPage = ({
+  uid,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+
+  // 로그인 된 유저가 로그인 페이지에 접근한 경우 home으로 보냄
+  if (uid) {
+    router.push('/');
+  }
+
   return (
     <div>
       <Head>
@@ -16,6 +32,25 @@ const SigninPage: NextPage = () => {
       </main>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = nookies.get(ctx);
+  const idToken = cookies.it;
+
+  try {
+    // idToken이 있다 = 로그인된 상태(로그인 한적이 없거나 로그아웃한 경우에는 쿠키에서 지울거기 때문에)
+    const decodedToken = await getAuth().verifyIdToken(idToken);
+
+    // 무결성 검사 성공한 경우 uid를 client로 전달
+    return {
+      props: { uid: decodedToken.uid },
+    };
+  } catch (error) {
+    return {
+      props: {},
+    };
+  }
 };
 
 export default SigninPage;
--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,9 +1,25 @@
 import { Box, Heading, Text } from '@chakra-ui/react';
-import type { NextPage } from 'next';
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next';
 import Head from 'next/head';
 import { Signup } from 'components/Auth';
+import { useRouter } from 'next/router';
+import nookies from 'nookies';
+import { getAuth } from 'firebase-admin/auth';
 
-const SignupPage: NextPage = () => {
+const SignupPage: NextPage = ({
+  uid,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+
+  // 로그인 된 유저가 회원가입 페이지에 접근한 경우 home으로 보냄
+  if (uid) {
+    router.push('/');
+  }
+
   return (
     <div>
       <Head>
@@ -18,4 +34,22 @@ const SignupPage: NextPage = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = nookies.get(ctx);
+  const idToken = cookies.it;
+
+  try {
+    // idToken이 있다 = 로그인된 상태(로그인 한적이 없거나 로그아웃한 경우에는 쿠키에서 지울거기 때문에)
+    const decodedToken = await getAuth().verifyIdToken(idToken);
+
+    // 무결성 검사 성공한 경우 uid를 client로 전달
+    return {
+      props: { uid: decodedToken.uid },
+    };
+  } catch (error) {
+    return {
+      props: {},
+    };
+  }
+};
 export default SignupPage;
